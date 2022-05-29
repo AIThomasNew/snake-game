b71603ab6ce8c723b11bba40612a48ae4b1d405d
Thomas <thomas.new.it@gmail.com>
2022-05-29 19:23:11 +0500

    clear

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
-// =========================== Переменные ===========================
-
-let scoreBlock; // отображение очков
-let score = 0; // очки внутри игры
-
+let scoreBlock // отображение очков
+let score = 0 // очки внутри игры
+ 
 // настройки игры
 const config = {
   step: 0, // пропуск игрового цикла
   maxStep: 6, // пропуск игрового цикла
   sizeCell: 16, // размер ячейки
   sizeBerry: 16 / 4, // размер ягоды
-};
+}
 
 // хранение характеристик змейки
 const snake = {
@@ -18,123 +16,122 @@ const snake = {
   dx: config.sizeCell, // скорость по горизонтали
   dy: 0, // скорость по вертикали
   tails: [], // массив ячеек под контролем моей змейки
-  maxTails: 3,
-};
+  maxTails: 3, // кол-во ячеек
+}
 
 // координаты ягоды
 let berry = {
   x: 0,
   y: 0,
-};
+}
 
-let canvas = document.querySelector('#game-canvas'); // получить канвас
-let context = canvas.getContext('2d');
-scoreBlock = document.querySelector('.game-score .score-count');
-drawScore();
+let canvas = document.querySelector('#game-canvas') // получить канвас
+let context = canvas.getContext('2d')
+scoreBlock = document.querySelector('.game-score .score-count')
+drawScore()
 
-// =========================== Функции ===========================
 
 // игровой цикл
 function gameLoop() {
-  requestAnimationFrame(gameLoop); // gameLoop будет выз. бесконечно
+  requestAnimationFrame(gameLoop) // gameLoop будет выз. бесконечно
 
   // значение из конфига step < maxStep, то пропуск дальнейшей работы ф.
   if (++config.step < config.maxStep) {
-    return;
+    return
   }
-  config.step = 0;
+  config.step = 0
 
   // каждый кадр очищать канвас
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height)
 
   // и заново отрисовывать все элементы
-  drawBerry(); // ягоду
-  drawSnake(); // змейку
+  drawBerry() // ягоду
+  drawSnake() // змейку
 }
-requestAnimationFrame(gameLoop); // gameLoop будет выз. бесконечно
+requestAnimationFrame(gameLoop) // gameLoop будет выз. бесконечно
 
 function drawSnake() {
   // изменение координат змейки согласно скорости
-  snake.x += snake.dx;
-  snake.y += snake.dy;
+  snake.x += snake.dx
+  snake.y += snake.dy
 
-  collisionBorder();
+  collisionBorder()
 
   // добавить в начало массива объект с 'x' и 'y' координатами
-  snake.tails.unshift({ x: snake.x, y: snake.y });
+  snake.tails.unshift({ x: snake.x, y: snake.y })
 
   // если кол-во дочерних элементов у змейки больше чем разрешено, то удалить последний элемент
   if (snake.tails.length > snake.maxTails) {
-    snake.tails.pop();
+    snake.tails.pop()
   }
 
   // перебрать все дочерние элементы у змейки, и отрисовываю их, попутно проверяя на соприкосновение их друг с другом и с ягодой
   snake.tails.forEach(function (el, index) {
     if (index == 0) {
-      context.fillStyle = '#00b7ff';
+      context.fillStyle = '#00b7ff'
     } else {
-      context.fillStyle = '#127aa3';
+      context.fillStyle = '#127aa3'
     }
-    context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell);
+    context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell)
 
     if (el.x === berry.x && el.y === berry.y) {
-      snake.maxTails++;
-      incScore();
-      randomPositionBerry();
+      snake.maxTails++
+      incScore()
+      randomPositionBerry()
     }
 
     for (let i = index + 1; i < snake.tails.length; i++) {
       if (el.x == snake.tails[i].x && el.y == snake.tails[i].y) {
-        refreshGame();
+        refreshGame()
       }
     }
-  });
+  })
 }
 
 function collisionBorder() {
   if (snake.x < 0) {
-    snake.x = canvas.width - config.sizeCell;
+    snake.x = canvas.width - config.sizeCell
   } else if (snake.x >= canvas.width) {
-    snake.x = 0;
+    snake.x = 0
   }
 
   if (snake.y < 0) {
     snake.y = canvas.height - config.sizeCell;
   } else if (snake.y >= canvas.height) {
-    snake.y = 0;
+    snake.y = 0
   }
 }
 function refreshGame() {
-  score = 0;
-  drawScore();
+  score = 0
+  drawScore()
 
-  snake.x = 160;
-  snake.y = 160;
-  snake.tails = [];
-  snake.maxTails = 3;
-  snake.dx = config.sizeCell;
-  snake.dy = 0;
+  snake.x = 160
+  snake.y = 160
+  snake.tails = []
+  snake.maxTails = 3
+  snake.dx = config.sizeCell
+  snake.dy = 0
 
-  randomPositionBerry();
+  randomPositionBerry()
 }
 
 function drawBerry() {
-  context.beginPath();
-  context.fillStyle = '#127aa3';
+  context.beginPath()
+  context.fillStyle = '#127aa3'
   context.arc(berry.x + config.sizeCell / 2, berry.y + config.sizeCell / 2, config.sizeBerry, 0, 2 * Math.PI);
-  context.fill();
+  context.fill()
 }
 
 // рандомное позиционирование ягоды
 function randomPositionBerry() {
-  berry.x = getRandomInt(0, canvas.width / config.sizeCell) * config.sizeCell;
-  berry.y = getRandomInt(0, canvas.height / config.sizeCell) * config.sizeCell;
+  berry.x = getRandomInt(0, canvas.width / config.sizeCell) * config.sizeCell
+  berry.y = getRandomInt(0, canvas.height / config.sizeCell) * config.sizeCell
 }
 
 // увеличивает значение очков на 1
 function incScore() {
-  score++;
-  drawScore();
+  score++
+  drawScore()
 }
 
 // отображает это значение на странице
@@ -150,16 +147,16 @@ function getRandomInt(min, max) {
 // добавить обработчик события и назначить управления клавишами
 document.addEventListener('keydown', function (event) {
   if (event.code == 'KeyW') {
-    snake.dy = -config.sizeCell;
-    snake.dx = 0;
+    snake.dy = -config.sizeCell
+    snake.dx = 0
   } else if (event.code == 'KeyA') {
-    snake.dx = -config.sizeCell;
-    snake.dy = 0;
+    snake.dx = -config.sizeCell
+    snake.dy = 0
   } else if (event.code == 'KeyS') {
-    snake.dy = config.sizeCell;
-    snake.dx = 0;
+    snake.dy = config.sizeCell
+    snake.dx = 0
   } else if (event.code == 'KeyD') {
-    snake.dx = config.sizeCell;
-    snake.dy = 0;
+    snake.dx = config.sizeCell
+    snake.dy = 0
   }
-});
+})
